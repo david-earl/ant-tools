1c8ffa4888541662a5736166bedac041b1b7edb6
David Earl <dquackenbush@illumina.com>
2014-08-27 20:04:53 +0000

    added AntReader class for programmatic access; fixed bug in command line arg handling

--- ant-tools.py
+++ ant-tools.py
@@ -1,33 +1,53 @@
 #!/usr/bin/python
 
 import io, os, sys, json
-
 from subprocess import Popen, PIPE
 
-BIN = "bin/ant-tools" 
+BIN = "ant-tools" 
+
+
+class AntReader(object):
+  def __init__(self, ant_path):
+    if not ant_path:
+      raise Exception("An .ant file must be specified")
+
+    self.ant_path = ant_path
+
+  def Load(self):
+    for line in call_bin(get_bin_path(), self.ant_path):
+      yield json.loads(line)
+
+  def Validate(self):
+    for line in call_bin(get_bin_path(), [self.ant_path, "--validate"]):
+      print line
+
+  def PrintStats(self):
+    for line in call_bin(get_bin_path(), [self.ant_path, "--stats"]):
+      print line
 
 
 def main():
-  cwd = os.getcwd()
-  filename = os.path.join(cwd, BIN) 
 
   args = sys.argv[1:]
 
   if not args:
     args = ['']
-  
 
-  for line in call_bin(filename, *args):
-    print json.dumps(record)
+  for line in call_bin(get_bin_path(), args):
+    if not line:
+      continue
+    print line
 
+def get_bin_path():
+  return os.path.join(os.getcwd(), BIN) 
 
 def call_bin(command, args):
-  command_args = [command, args]
+  command_args = [command] + args
 
   process = Popen(command_args, stdout=PIPE)
 
   for line in io.open(process.stdout.fileno()):
-    yield ine.rstrip('\n')
+    yield line.rstrip('\n')
 
 
 if __name__ == '__main__':
